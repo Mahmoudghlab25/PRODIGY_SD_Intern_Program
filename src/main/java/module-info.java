f93e25b0453db6ff1a424e16e60ec858ee1b3dfb
Mahmoud Ghlab <96776435+Mahmoudjobdis@users.noreply.github.com>
2024-02-03 04:12:22 +0200

    reformat

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.prodigy_intern_program {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.prodigy_intern_program to javafx.fxml;
-    exports com.example.prodigy_intern_program;
-}
